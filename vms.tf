5d9098c6ae015414e652f41500e32b68890641b0
sachin <sachinv_k@yahoo.com>
2023-10-08 11:44:14 +0000

    Initial commit

---
+++ vms.tf
@@ -0,0 +1,26 @@
+# main.tf
+
+provider "aws" {
+  region = "your-region"
+}
+
+resource "aws_instance" "micro_instance_1" {
+  ami           = "ami-xxxxxxxxxxxxxxxx"  # replace with the desired AMI ID
+  instance_type = "t2.micro"
+  count         = 2  # create two t2.micro instances
+}
+
+resource "aws_instance" "large_instance_1" {
+  ami           = "ami-xxxxxxxxxxxxxxxx"  # replace with the desired AMI ID
+  instance_type = "t2.large"
+  count         = 1  # create one t2.large instance
+}
+
+output "micro_instance_ips" {
+  value = aws_instance.micro_instance_1[*].public_ip
+}
+
+output "large_instance_ips" {
+  value = aws_instance.large_instance_1[*].public_ip
+}
+
